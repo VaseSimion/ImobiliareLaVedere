bda9a8be545bd8bb97342fc19a764b20552e5488
VaseSimion <sularea.vasile@gmail.com>
2023-01-20 20:59:24 +0100

    created functions to run all pages in the utilities file created a requests version with bs4 instead of selenium (is much faster) left some test utilities for future use in utilities Simplified workfile by moving the run function

--- ScrapingUtilities.py
+++ ScrapingUtilities.py
@@ -1,13 +1,17 @@
-from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import requests as re
+from bs4 import BeautifulSoup
+import bs4
+import pandas as pd
 
 
-def decode_announcement_imobiliare(dwelling_element: WebElement):
+def decode_announcement_imobiliare_selenium(dwelling_element: WebElement):
     dwelling = dict()
     dwelling["data_integrity"] = True
     if dwelling_element.get_attribute("class") == "box-anunt proiect standard":
@@ -18,7 +22,6 @@ def decode_announcement_imobiliare(dwelling_element: WebElement):
     dwelling["judet"] = dwelling_element.get_attribute("data-judet")
     dwelling["zona"] = dwelling_element.get_attribute("data-zona")
     dwelling["NoCamere"] = dwelling_element.get_attribute("data-camere")
-    dwelling["Suprafata"] = dwelling_element.get_attribute("data-surface")
     dwelling["Type"] = dwelling_element.get_attribute("data-name")
     dwelling["InternalID"] = dwelling_element.get_attribute("id")
 
@@ -60,11 +63,111 @@ def decode_announcement_imobiliare(dwelling_element: WebElement):
         elif "camere" in split_element[2]:
             dwelling["Partajare"] = split_element[3][split_element[3].find("<strong>")
                                                      + 8:split_element[3].find("</strong>")]
-        else:
-            dwelling["error"] = True
+        elif "suprafata" in split_element[2]:
+            if len(split_element) == 6:
+                dwelling["Suprafata"] = split_element[4][split_element[4].find("<strong>")+8:split_element[4].find("</strong")]
+            if len(split_element) == 5:
+                dwelling["data_integrity"] = False
+    return dwelling
+
+
+def decode_announcement_imobiliare_beautiful(dwelling_element: bs4.element.Tag):
+    dwelling = dict()
+    attribute_dictionary = dwelling_element.attrs
+
+    if len(attribute_dictionary["class"]) != 1:
+        dwelling["data_integrity"] = False
+        return dwelling
+
+    dwelling["pret"] = attribute_dictionary["data-price"]
+
+    caracteristici = dwelling_element.find(class_="swiper-wrapper").find_all(class_="caracteristica")
+    dwelling["suprafata"] = " ".join(caracteristici[1].find('span').text.split()).split()[0]
+
+    dwelling["etaj"] = " ".join(caracteristici[2].find('span').text.split())
+    if len(caracteristici) > 3:
+        dwelling["partajare"] = " ".join(caracteristici[3].find('span').text.split())
+    else:
+        dwelling["partajare"] = "Not specified"
+    dwelling["noCamere"] = attribute_dictionary["data-camere"]
+    dwelling["type"] = attribute_dictionary["data-name"]
+
+    dwelling["judet"] = attribute_dictionary["data-judet"]
+    locatie = " ".join(dwelling_element.find(class_="location_txt").text.split("\n")[2].split())
+    if "," in locatie:
+        dwelling["oras"] = locatie.split(",")[0]
+        dwelling["zona"] = locatie.split(",")[1].replace("ş", "s").replace("ă", "a").replace("Î", "I").replace("â","a")
+    else:
+        dwelling["oras"] = locatie
+        dwelling["zona"] = "Nan"
+
+    if len(dwelling_element.find(class_="comision").text.split("\n")) == 3:
+        dwelling["comision"] = " ".join(dwelling_element.find(class_="comision").text.split("\n")[2].split())
+    else:
+        dwelling["comision"] = "Standard"
+
+    if "TVA" in dwelling_element.find(class_="tva-luna").text:
+        dwelling["tva"] = "Yes"
+    else:
+        dwelling["tva"] = "No"
+
+    dwelling["anunt"] = dwelling_element.find(class_="titlu-anunt").text.split("\n")[1]
+    dwelling["internalID"] = attribute_dictionary["id"]
+    dwelling["data_integrity"] = True
 
     return dwelling
 
 
+def run_page_selenium(page_no):
+    local_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    local_dataframe = pd.DataFrame()
+    local_driver.get("https://www.imobiliare.ro/vanzare-apartamente/cluj-napoca?pagina={}".format(page_no))
+    WebDriverWait(local_driver, 60).until(ec.presence_of_element_located((By.CLASS_NAME, "box-anunt")))
+    listings = local_driver.find_elements(By.CLASS_NAME, "box-anunt")
+    for listing in listings:
+        apartment_dict = decode_announcement_imobiliare_selenium(listing)
+        #print(apartment_dict)
+        if apartment_dict["data_integrity"]:
+            df_dictionary = pd.DataFrame([apartment_dict])
+            local_dataframe = pd.concat([local_dataframe, df_dictionary], ignore_index=True)
+    local_driver.close()
+    return local_dataframe
+
+
+def run_page_beautifulsoup(page_no):
+    local_dataframe = pd.DataFrame()
+    url = "https://www.imobiliare.ro/vanzare-apartamente/cluj-napoca?pagina={}".format(page_no)
+    page_html = re.get(url)
+    page_soup = BeautifulSoup(page_html.content, 'html.parser')
+    listings = page_soup.find_all(class_="box-anunt")
+    for listing in listings:
+        apartment_dict = decode_announcement_imobiliare_beautiful(listing)
+        if apartment_dict["data_integrity"]:
+            df_dictionary = pd.DataFrame([apartment_dict])
+            local_dataframe = pd.concat([local_dataframe, df_dictionary], ignore_index=True)
+    return local_dataframe
+
+
 if __name__ == "__main__":
-    pass
+    base_url = "https://www.imobiliare.ro/vanzare-apartamente/cluj-napoca"
+    page = re.get(base_url)
+    page_soup = BeautifulSoup(page.content, 'html.parser')
+    anunturi = page_soup.find(class_="ultima butonpaginare double")
+    attribute_dictionary = anunturi.attrs
+    print(attribute_dictionary["data-pagina"])
+
+
+#  print(anunturi[0].find(class_="tva-luna").text.split("\n"))
+
+    # for anunt in anunturi:
+    #     result =decode_announcement_imobiliare_beautiful(anunt)
+    #     if result["data_integrity"]:
+    #         print(result["pret"])
+
+
+    # result = decode_announcement_imobiliare_beautiful(anunturi[0])
+    # for element in result:
+    #     print(element+":", result[element])
+
+
+
--- Workfile.py
+++ Workfile.py
@@ -1,23 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By
 import ScrapingUtilities as Su
+import time
+from concurrent.futures import ThreadPoolExecutor
+import requests as re
+from bs4 import BeautifulSoup
 import pandas as pd
 
 
 complete_dataframe = pd.DataFrame()
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.imobiliare.ro/vanzare-apartamente/cluj")
-WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.CLASS_NAME, "box-anunt")))
-listings = driver.find_elements(By.CLASS_NAME, "box-anunt")
-for listing in listings:
-    apartment_dict = Su.decode_announcement_imobiliare(listing)
-    if apartment_dict["data_integrity"]:
-        df_dictionary = pd.DataFrame([apartment_dict])
-        complete_dataframe = pd.concat([complete_dataframe, df_dictionary], ignore_index=True)
-driver.close()
 
-print(complete_dataframe)
+base_url = "https://www.imobiliare.ro/vanzare-apartamente/cluj-napoca"
+page_soup = BeautifulSoup(re.get(base_url).content, 'html.parser')
+anunturi = page_soup.find(class_="ultima butonpaginare double")
+attribute_dictionary = anunturi.attrs
+pagina_max = int(attribute_dictionary["data-pagina"])
+
+with ThreadPoolExecutor(max_workers=100) as p:
+    start_time = time.time()
+    results = p.map(Su.run_page_beautifulsoup, range(1, pagina_max+1))
+    for result in results:
+        complete_dataframe = pd.concat([result, complete_dataframe], ignore_index=True)
+    print(f"{(time.time() - start_time):.2f} seconds")
+
+# for i in range(1, 21):
+#     print("Run number:", i)
+#     start_time = time.time()
+#     result = Su.run_page_beautifulsoup(i)
+#     complete_dataframe = pd.concat([result, complete_dataframe], ignore_index=True)
+#     print(f"{(time.time() - start_time):.2f} seconds")
+
+complete_dataframe.to_csv('20Jan.csv')
